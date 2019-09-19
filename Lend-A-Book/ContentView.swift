df9673590fd84ed1f6b5523ecdf8bc529149b778
Brett Chapin <blackroseangel@gmail.com>
2019-09-18 23:29:47 -0400

    Initial Commit

---
+++ Lend-A-Book/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lend-A-Book
+//
+//  Created by Brett Chapin on 9/18/19.
+//  Copyright © 2019 Black Rose Productions. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
