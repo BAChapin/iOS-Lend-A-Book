176bbd820bf8d8ce4b5bfb821265f8869b35bef5
BAChapin <brett.chapin89@gmail.com>
2020-07-20 11:16:56 -0400

    Initial Commit

---
+++ Lend-A-Book/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Lend-A-Book
+//
+//  Created by Brett Chapin on 7/20/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lend-A-Book/Lend_A_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lend_A_BookApp.swift
+//  Lend-A-Book
+//
+//  Created by Brett Chapin on 7/20/20.
+//
+
+import SwiftUI
+
+@main
+struct Lend_A_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
